e1200c380e1e20d926f222c9ae29f6c2a6fa874b
melio0o <mtsucordeiro0318@gmail.com>
2023-09-27 22:25:05 -0300

    a

---
+++ Assets/scripts/Inventory.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (fileName = "Inventory", menuName = "ScriptableAssets/New Inventory")]
+
+public class Inventory : ScriptableObject
+{
+    [SerializeField] public Item[] items;
+}
